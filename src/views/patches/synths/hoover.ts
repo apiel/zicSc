3dc0a5616626353b1c2d9e4f977939d592abbb41
Alexandre Piel <Alexandre.Piel@zooplus.com>
2023-04-02 17:43:35 +0200

    hoover

---
+++ src/views/patches/synths/hoover.ts
@@ -0,0 +1,27 @@
+import { Encoders } from '../../../layout/encoders.layout';
+import { SynthData } from './SynthData';
+
+const defaultValue = {};
+
+const encoders: Encoders = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+
+const main = {
+    encoders,
+};
+
+export default new SynthData(
+    'hoover',
+    `SynthDef("hoover", {| note = 60 |
+        var snd, freq, bw, delay, decay;
+        freq = note.midicps;
+        freq = freq * Env([-5, 6, 0], [0.1, 1.7], [\\lin, -4]).kr.midiratio;
+        bw = 1.035;
+        snd = { DelayN.ar(Saw.ar(freq * ExpRand(bw, 1 / bw)) + Saw.ar(freq * 0.5 * ExpRand(bw, 1 / bw)), 0.01, Rand(0, 0.01)) }.dup(20);
+        snd = (Splay.ar(snd) * 3).atan;
+        snd = snd * Env.asr(0.01, 1.0, 1.0).kr(0, \\gate.kr(1));
+        snd = FreeVerb2.ar(snd[0], snd[1], 0.3, 0.9);
+        snd = snd * Env.asr(0, 1.0, 4, 6).kr(2, \\gate.kr(1));
+        Out.ar(\\out.kr(0), snd * \\amp.kr(0.1));
+    })`,
+    [main],
+);
--- src/views/patches/synths/index.ts
+++ src/views/patches/synths/index.ts
@@ -2,12 +2,14 @@ import psykick from './psykick';
 import bubble from './bubble';
 import sikGoo from './sik-goo';
 import gabberkick from './gabberkick';
+import hoover from './hoover';
 
 export const synths = [
     sikGoo, 
     psykick, 
     bubble, 
     gabberkick,
+    hoover
 ];
 export const synthsMap = new Map(synths.map((synth) => [synth.name, synth]));
 export const synthsNames = Array.from(synthsMap.keys());
